b728522a6419d7728db69a21479a04ce7733deb3
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-10 20:15:04 -0400

    added button constructor

---
+++ buttons.js
@@ -0,0 +1,46 @@
+function create(element) {
+  return document.createElement(element);
+}
+
+const root = document.querySelector("#root");
+
+function ButtonMaker(textContent, linkLocation) {
+  const buttonLink = create("a");
+  const buttonContainer = create("div");
+  const stepParent = create("div");
+  const spanOne = create("span");
+  const spanTwo = create("span");
+  const spanThree = create("span");
+  const spanFour = create("span");
+  const button = create("div");
+  const text = create("div");
+
+  buttonLink.classList.add("button-link");
+  buttonContainer.classList.add("button-container");
+  stepParent.classList.add("span-parent");
+  button.classList.add("button-filter");
+  text.classList.add("button-text");
+
+  buttonLink.href = linkLocation;
+  text.textContent = textContent;
+
+  buttonLink.appendChild(buttonContainer);
+  buttonContainer.appendChild(stepParent);
+  stepParent.appendChild(spanOne);
+  stepParent.appendChild(spanTwo);
+  stepParent.appendChild(spanThree);
+  stepParent.appendChild(spanFour);
+  stepParent.appendChild(button);
+  button.appendChild(text);
+
+  return buttonLink;
+}
+
+function SmallerButton(textContent, linkLocation, x, y, minWid) {
+  const newButton = ButtonMaker(textContent, linkLocation);
+  newButton.setAttribute("style", `transform: scale(${x}, ${y})`);
+  newButton.style.minWidth = minWid;
+  newButton.classList.add("small-button");
+
+  return newButton;
+}
